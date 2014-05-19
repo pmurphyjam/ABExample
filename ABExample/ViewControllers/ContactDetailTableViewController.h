7c7f518fa1e16bc15261f0f0d809637e3a032dec
Pat Murphy <pmurphyjam@me.com>
2014-05-18 20:58:23 -0700

    Added Contact Detail and Contacts TableViewControllers

---
+++ ABExample/ViewControllers/ContactDetailTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ContactDetailTableViewController.h
+//  ABExample
+//
+//  Created by Pat Murphy on 5/18/14.
+//  Copyright (c) 2014 Pat Murphy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContactDetailTableViewController : UITableViewController
+
+@end
